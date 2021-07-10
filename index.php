283f57b828630790eb9c3d0333598260ed345e55
Lucho Web <luchoweb.dev@gmail.com>
2021-07-09 22:18:00 -0500

    autoload file created (#1)

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+/**
+ * Bootstrap File
+ * @author LUCHOWEB
+ */
+
+spl_autoload_register(function($class){
+    $pathFile = str_replace("\\", "/", $class) . ".php";
+
+    if ( is_readable($pathFile) ){
+        require_once $pathFile;
+    } else {
+        print "Ha ocurrido un error.";
+    }
+});
+
+require_once "config.php";
+require_once "routes.php";
