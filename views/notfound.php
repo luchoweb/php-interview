4f974a6c29dee56998bf2cf528c8d026a44dd019
Lucho Web <webfacilco@gmail.com>
2021-07-09 23:36:13 -0500

    route and controller done

--- views/notfound.php
+++ views/notfound.php
@@ -7,7 +7,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Not found!</title>
+  <title>Error: 404 - Not found!</title>
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="assets/css/not-found.css">
